339697e8e6abae8f09c3346625517c10f88194a2
Josh Rackers <jrackers@wustl.edu>
2018-01-23 14:06:16 -0600

    adds docs to simpsons rule implementation

--- integrate/newton_cotes/simpson.py
+++ integrate/newton_cotes/simpson.py
@@ -4,9 +4,31 @@ This file contains the implementation of the Simpson rule
 
 import numpy as np
 
-def evaluate(x, f):
-    a = x[0]
-    b = x[1]
+def evaluate(bounds, f):
+    """
+
+    Evaluate simpson's rule on an array of values and a function pointer
+    \int_{a}^{b} = \sum_i ...
+
+    Parameters
+    ----------
+    bounds : array_like
+        An array with a dimension of two that contains the starting and ending points of the integrand.
+    f : function
+        Evaluates the integrand at a point
+
+    Returns
+    -------
+    integral : float
+        The integral of the function between the bounds.
+
+    """
+
+    if len(bounds) != 2:
+        raise ValueError("Bounds should be a length of two, found %d." % len(bounds))
+
+    a = bounds[0]
+    b = bounds[1]
     ya = f(a)
     yb = f((a+b)/2.0)
     yc = f(b)
